1267d27d5bb8078e772e17c222a380743057850c
unknown <helinnozalkan@gmail.com>
2024-08-16 11:12:37 +0300

    post güncellendi

--- chatbotapi/src/api.js
+++ chatbotapi/src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:5173';
+const API_BASE_URL = 'http://localhost:5204';
 
 // Seyahat bilgilerini almak için fonksiyon
 export const fetchChatbotResponse = async (destination, language) => {
